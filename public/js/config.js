a47caf25e7e35643faa9a4cbafcc3305b9fffdcc
Mateus Chagas <contato@mateuschagas.com.br>
2013-06-19 22:36:56 -0300

    Ajustando o template!

--- public/js/config.js
+++ public/js/config.js
@@ -30,8 +30,8 @@ jQuery(function() {
 		if (jQuery.browser.msie && jQuery.browser.version <= 9)
 			jQuery('form').n33_formerize();
 
-		jQuery('form .form-button-submit').click(function(e) { e.preventDefault(); jQuery(this).closest('form').submit(); });
-		jQuery('form .form-button-reset').click(function(e) { e.preventDefault(); jQuery(this).closest('form')[0].reset(); });
+		/*jQuery('form .form-button-submit').click(function(e) { e.preventDefault(); jQuery(this).closest('form').submit(); });
+		jQuery('form .form-button-reset').click(function(e) { e.preventDefault(); jQuery(this).closest('form')[0].reset(); });    */
 	
 	// Links
 		jQuery('a').click(function(e) {
